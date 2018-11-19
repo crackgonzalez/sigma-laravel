88ff4599b8862696c11959a6782b6dbfa9d74b80
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2018-11-19 15:30:05 -0300

    implementacion de carousel en pagina de inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -12,7 +12,7 @@
 		</ul>	
 		<form class="form-inline">
 			<input class="form-control mr-sm-2 my-2" type="search" placeholder="Correo" aria-label="Correo" />
-			<input class="form-control mr-sm-2 my-2" type="search" placeholder="Contraseña" aria-label="Contraseña" />	
+			<input class="form-control mr-sm-2 my-2" type="search" placeholder="Contraseña" aria-label="Contraseña" />
         	<button class="btn btn-outline-info my-2" type="submit">Iniciar Sesión</button>
         	<button class="btn btn-outline-info my-2" style="margin-left: 10px;" type="submit">Registrarse</button>
 		</form>
@@ -20,12 +20,49 @@
 </nav>
 @endsection
 @section('contenido')
- <div class="row">
-		<div class="col-sm-6">
-			<h1>Prueba de seccion contenido</h1>
-		</div>
-		<div class="col-sm-6">
-			<h1>Prueba de seccion contenido</h1>
+<div class="row">
+	<div class="col-sm-12">
+		<div id="carousel" class="carousel slide" data-ride="carousel" style="margin-right: -15px; margin-left: -15px;">
+			<ol class="carousel-indicators">
+				<li data-target="#carousel" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel" data-slide-to="1"></li>
+				<li data-target="#carousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img width="100%" src="{{asset('img/inicio1.jpg')}}" alt="First slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Servicios</h5>
+						<p>Texto de prueba texto de prueba</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img width="100%" src="{{asset('img/inicio2.jpg')}}" alt="Second slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Rotaciones</h5>
+						<p>Texto de prueba texto de prueba</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img width="100%" src="{{asset('img/inicio3.jpg')}}" alt="Third slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Informacion</h5>
+						<p>Texto de prueba texto de prueba</p>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 	</div>
+</div>
 @endsection
+
+
+<!--  -->
